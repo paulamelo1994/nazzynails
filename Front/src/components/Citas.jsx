c3f505d8b0f1ded652bd2d0685e426fad36a1dd9
paulamelo <paudamelo@gmail.com>
2021-09-03 01:03:24 -0500

    Añadir calendario

--- Front/src/components/Citas.jsx
+++ Front/src/components/Citas.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
 
 const Citas = () => {
-    return <p>Soy la pagina de inicio</p>
+
+    const [date, setDate] = useState(new Date());
+    return (
+        <div>
+
+            <p>Soy la pagina de inicio</p>
+            <Calendar
+                onChange={setDate}
+                value={date}
+            />
+            {console.log(date)}
+
+        </div>
+    );
 
 }
 
