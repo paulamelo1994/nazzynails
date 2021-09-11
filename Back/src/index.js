5644ed164cb41c80d1a2d2314d3b7e73c3feda7c
Daniel Luna <daniel.luna@correounivalle.edu.co>
2021-09-11 00:36:44 -0500

    User added

--- Back/src/index.js
+++ Back/src/index.js
@@ -1,22 +1,26 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+require('rootpath')();
+const bodyParser = require('body-parser');
+const errorHandler = require('./_middleware/error-handler');
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cors())
 
 // Coloca el nombre del archivo que contiene sus respectivas rutas
-const routes = [
-    'cliente',
-    'cita',
-    'usuario'
-];
+//app.use('/users', require('./users/users.controller'));
+
+app.use(errorHandler);
 
 // Se importan cada una de las rutas
-app.use('/api', routes.map(r => require(`./routes/${r}`)))
+//app.use('/api', routes.map(r => require(`./routes/${r}`)))
+
+app.use('/api/users', require('./controllers/users.controller'));
+
 const server = app.listen(
     PORT,
     () => console.log('Servidor iniciado en el puerto', PORT))
