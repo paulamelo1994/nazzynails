c8a885b90cf81aefa91cf6cc59f0405282ead4a5
baiest <ballesteros.juan@correounivalle.edu.co>
2021-09-13 16:36:19 -0500

    Archivos inecesarios borrados

--- Back/src/routes/cita.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const path = 'date'
-
-router.get(`/${path}/`, (req, res) => res.send({ message: 'Reservar cita' }))
-router.get(`/${path}/cancel`, (req, res) => res.send({ message: 'Cancelar cita' }))
-
-module.exports = router
